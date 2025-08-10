fafa1cdc4fd31ad7afc8d35e8e960e93b4f2f1fa
LeonardoDiazz <20233tn141@utez.edu.mx>
2025-08-09 23:36:13 -0600

    Modifique el tamaño de la ventana

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -1,5 +1,4 @@
 package com.example.demo;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -11,7 +10,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 750, 600);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
@@ -20,4 +19,5 @@ public class HelloApplication extends Application {
     public static void main(String[] args) {
         launch();
     }
+
 }
