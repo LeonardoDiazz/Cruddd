ad071bc190f9e7ad4d2955e519ec273d4491a5e7
LeonardoDiazz <20233tn141@utez.edu.mx>
2025-08-09 23:25:12 -0600

    Cree la clase crud java

--- src/main/java/com/example/demo/Crud.java
+++ src/main/java/com/example/demo/Crud.java
@@ -1,4 +1,60 @@
 package com.example.demo;
 
+
+import java.time.LocalDate;
+
 public class Crud {
+    private int id;
+    private String titulo;
+    private String descripcion;
+    private String prioridad;
+    private LocalDate fechaLimite;
+
+    public Crud(int id, String titulo, String descripcion, String prioridad, LocalDate fechaLimite) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.prioridad = prioridad;
+        this.fechaLimite = fechaLimite;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getPrioridad() {
+        return prioridad;
+    }
+
+    public void setPrioridad(String prioridad) {
+        this.prioridad = prioridad;
+    }
+
+    public LocalDate getFechaLimite() {
+        return fechaLimite;
+    }
+
+    public void setFechaLimite(LocalDate fechaLimite) {
+        this.fechaLimite = fechaLimite;
+    }
 }
